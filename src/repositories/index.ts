a07472f5c1eb97e987387fbdbb78dbd63fddb9dc
Daniel Felipe Franco Rincón <daniel.franco.rincon@gmail.com>
2023-03-14 01:07:36 -0500

    Repositories creation

---
+++ src/repositories/index.ts
@@ -0,0 +1,5 @@
+export * from './login.repository';
+export * from './menu-role.repository';
+export * from './menu.repository';
+export * from './role.repository';
+export * from './user.repository';
---
+++ src/repositories/menu-role.repository.ts
@@ -0,0 +1,16 @@
+import {inject} from '@loopback/core';
+import {DefaultCrudRepository} from '@loopback/repository';
+import {MongodbDataSource} from '../datasources';
+import {MenuRole, MenuRoleRelations} from '../models';
+
+export class MenuRoleRepository extends DefaultCrudRepository<
+  MenuRole,
+  typeof MenuRole.prototype._id,
+  MenuRoleRelations
+> {
+  constructor(
+    @inject('datasources.mongodb') dataSource: MongodbDataSource,
+  ) {
+    super(MenuRole, dataSource);
+  }
+}
