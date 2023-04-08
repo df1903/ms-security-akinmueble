13a130f323fdeb07fcac2c24f3ff7f1c94b42a0e
Zack <99695700+Zack4U@users.noreply.github.com>
2023-04-08 18:08:14 -0500

    Change [urlNotifications] http protocol

--- src/config/notifications.config.ts
+++ src/config/notifications.config.ts
@@ -1,7 +1,7 @@
 export namespace NotificationsConfig {
   export const emailSubject2FA = 'Verification Code';
   export const urlNotifications2FA: string =
-    'https://localhost:7183/Notifications/send-code-2fa';
+    'http://localhost:7183/Notifications/send-code-2fa';
   export const urlNotificationsSMS: string =
-    'https://localhost:7183/Notifications/send-sms';
+    'http://localhost:7183/Notifications/send-sms';
 }
